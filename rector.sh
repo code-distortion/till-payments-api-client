76888ece022510801a89110cc8bcab975f8bbea1
Tim Chandler <tim@code-distortion.net>
2022-01-27 17:13:04 +1100

    Changed Rector to downgrade to PHP 7.1 instead of 7.0

--- rector.sh
+++ rector.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cd /var/www/html/till-payments-php7 || exit
-mv vendor ..
-rm -rf * .*
-cp -pr /var/www/html/till-payments/. /var/www/html/till-payments-php7
-rm -rf _src _tests
-
-git add . --all
-git commit -m "WIP"
-
 cp -pr src _src
 cp -pr tests _tests
 
@@ -27,6 +18,3 @@ rm -rf src tests
 
 mv _src src
 mv _tests tests
-
-rm -rf vendor
-mv ../vendor .

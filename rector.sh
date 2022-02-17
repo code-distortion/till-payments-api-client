9d3e37223e2c1c3e825eb8c47d6c0276a4229dcb
Tim Chandler <tim@code-distortion.net>
2022-02-17 13:42:43 +1100

    Updated the rector scripts used to transliterate to older versions of PHP - to avoid problems, this ensures Rector's cache is clear

--- rector.sh
+++ rector.sh
@@ -3,7 +3,7 @@
 cp -pr src _src
 cp -pr tests _tests
 
-./vendor/bin/rector process _src _tests
+./vendor/bin/rector process --clear-cache _src _tests
 ./vendor/bin/phpcbf _src _tests
 #sed -i 's/public function setUp()/public function setUp(): void/' _tests/Browser/LaravelBrowserTest.php
 #sed -i 's/public function setUp()/public function setUp(): void/' _tests/LaravelDuskTestCase.php
